017ecc3a324c0e6f27b7d2ae525d715d741256c6
Adam <adam.waniak1@gmail.com>
2018-08-26 23:11:20 +0200

    Secure manage-quiz

--- src/main/webapp/app/features/quiz-manager/quiz-manager-routing.module.ts
+++ src/main/webapp/app/features/quiz-manager/quiz-manager-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from 'app/features/quiz-manager/quiz-list/quiz-list.component';
 import { QuizTaskSetListComponent } from 'app/features/quiz-manager/quiz-task-set-list/quiz-task-set-list.component';
 import { QuizManagerContainerComponent } from 'app/features/quiz-manager/container/quiz-manager-container.component';
+import { UserRouteAccessService } from 'app/core';
 
 const routes: Routes = [
     {
         path: '',
         component: QuizManagerContainerComponent,
+        data: {
+            authorities: ['ROLE_USER'],
+            defaultSort: 'id,asc',
+            pageTitle: 'Quizzes'
+        },
+        canActivate: [UserRouteAccessService],
         children: [
             {
                 path: '',
