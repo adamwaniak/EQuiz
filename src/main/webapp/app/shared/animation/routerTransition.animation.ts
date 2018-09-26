1f19d9351c8109ca126f8cb58a7e3f6ef2ea607e
Adam Waniak <adam.waniak@volvo.com>
2018-09-26 14:46:17 +0200

    Changed transition animation time, changed min screen height

--- src/main/webapp/app/shared/animation/routerTransition.animation.ts
+++ src/main/webapp/app/shared/animation/routerTransition.animation.ts
@@ -1,12 +1,16 @@
-import {animate, query, style, transition, trigger} from '@angular/animations';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 
 export const routerTransition =
     // trigger name for attaching this animation to an element using the [@triggerName] syntax
     trigger('routerTransition', [
         transition('* => quiz-manager', [
-            query(':enter', [style({opacity: 0}), animate('0.3s', style({opacity: 0})), animate('0.5s', style({opacity: 1}))], {
+            query(':enter', [style({ opacity: 0 }), animate('0.3s', style({ opacity: 0 })), animate('0.5s', style({ opacity: 1 }))], {
                 optional: true
             })
         ]),
-        transition('* <=> *', [query(':enter', [style({ opacity: 0 }), animate('0.5s', style({ opacity: 1 }))], { optional: true })])
+        transition('* <=> *', [
+            query(':enter', [style({ opacity: 0 }), animate('0.3s', style({ opacity: 0 })), animate('0.5s', style({ opacity: 1 }))], {
+                optional: true
+            })
+        ])
     ]);
