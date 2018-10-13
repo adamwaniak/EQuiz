972dbffcfd368bd2e2862ce8b7e46699bc93a73b
Adam <adam.waniak1@gmail.com>
2018-10-13 12:59:53 +0200

    Changed design of navbar and home page

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-
-import { LoginModalService, Principal, Account } from 'app/core';
+import { faEdit, faFeatherAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Account, LoginModalService, Principal } from 'app/core';
 
 @Component({
     selector: 'jhi-home',
@@ -12,6 +12,9 @@ import { LoginModalService, Principal, Account } from 'app/core';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    faFeatherAlt = faFeatherAlt;
+    faEdit = faEdit;
+    faSearch = faSearch;
 
     constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
 
